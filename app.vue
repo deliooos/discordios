1b039748f3e778e4522814238e2792d36f31af62
Loeiz Moynet <moynetloeiz@gmail.com>
2022-12-06 16:34:51 +0100

    Added the Router in App

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center items-center w-full h-screen bg-slate-800">
-    <h1 class="font-bold text-5xl text-white">Hey, TailwindCSS !</h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
