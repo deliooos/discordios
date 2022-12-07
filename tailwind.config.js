6006f5cee87b600de6453a64948d9acb3469fdad
delios <moynetloeiz@gmail.com>
2022-12-07 23:57:02 +0100

    Test colors, still have stuff to improve

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 export default {
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary': {
+                    900: '#002418',
+                    700: '#0e4935',
+                    500: '#19e6a2',
+                    300: '#8ffad6',
+                    100: '#e7fdf6',
+                },
+            },
+        },
     },
     plugins: [],
 }
